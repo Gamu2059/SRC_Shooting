e74c2abc9c6315305807d0150f623a9a99da3ba3
Gamu2059 <yamagami0711syo@gmail.com>
2019-02-14 05:32:24 +0900

    Add : ゲームのベース部分の準備

---
+++ SRC_Shooting/Assets/Scripts/Systems/Scene/BaseScene.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class BaseScene : MonoBehaviour
+{
+
+	[SerializeField]
+	private BaseSceneManager.E_SCENE m_Scene;
+
+	public BaseSceneManager.E_SCENE Scene
+	{
+		get
+		{
+			return m_Scene;
+		}
+	}
+
+
+	private void Awake()
+	{
+
+		if( !BaseSceneManager.CheckExistInstance() )
+		{
+			SceneManager.LoadScene( 0 );
+			return;
+		}
+
+		BaseSceneManager.Instance.RegisterScene( this );
+	}
+
+	public virtual void OnBeforeHide( Action onComplete = null )
+	{
+		EventUtility.SafeInvokeAction( onComplete );
+	}
+
+	public virtual void OnAfterHide( Action onComplete = null )
+	{
+		EventUtility.SafeInvokeAction( onComplete );
+	}
+
+	public virtual void OnBeforeShow( Action onComplete = null )
+	{
+		EventUtility.SafeInvokeAction( onComplete );
+	}
+
+	public virtual void OnAfterShow( Action onComplete = null )
+	{
+		EventUtility.SafeInvokeAction( onComplete );
+	}
+}
