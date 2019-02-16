580437f1d2e1ebf2cfea0a8f49e345fda1e3ffe5
Gamu <yamagami0711syo@gmail.com>
2019-02-16 15:02:22 +0900

    Revert "Feature/yamagami/chara"

--- SRC_Shooting/Assets/Scripts/Systems/Singleton/GlobalSingletonMonoBehavior.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class GlobalSingletonMonoBehavior<T> : SingletonMonoBehavior<T> where T : MonoBehaviour
-{
-	protected override void OnAwake()
-	{
-		DontDestroyOnLoad( gameObject );
-	}
-}
