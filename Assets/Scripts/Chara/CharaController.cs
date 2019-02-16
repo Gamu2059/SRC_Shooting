36d272287b4c63973597ed9babf2cb1cdc2369e8
mstsh7 <hayakawagt400@gmail.com>
2019-02-16 18:18:35 +0900

    Add : SmasherBullet Script.

--- Assets/Scripts/Chara/CharaController.cs
+++ Assets/Scripts/Chara/CharaController.cs
@@ -22,6 +22,29 @@ public class CharaController : MonoBehaviour
 	private KeyCode m_RightKey;
 
 	[SerializeField]
+	private KeyCode m_ShotKey;
+
+	[SerializeField]
+	private GameObject m_Bullet;
+
+	[SerializeField]
+	private float m_ShotInterval;
+
+	private float shotDelay;
+
+	[SerializeField]
+	private Transform[] m_MainShotPosition;
+
+	[SerializeField]
+	private Transform[] m_SubShotPosition2;
+
+	[SerializeField]
+	private Transform[] m_SubShotPosition3;
+
+	[SerializeField, Range(1,3)]
+	private int m_ShotLevel;
+
+	[SerializeField]
 	private Transform[] m_Protectors;
 
 	[SerializeField]
@@ -34,6 +57,8 @@ public class CharaController : MonoBehaviour
 
 	private void Update()
 	{
+		shotDelay += Time.deltaTime;
+
 		if( Input.GetKey( m_FrontKey ) )
 		{
 			transform.Translate( Vector3.forward * m_MoveSpeed * Time.deltaTime );
@@ -54,6 +79,13 @@ public class CharaController : MonoBehaviour
 			transform.Translate( Vector3.right * m_MoveSpeed * Time.deltaTime );
 		}
 
+		if(Input.GetKey(m_ShotKey)){
+			if(shotDelay >= m_ShotInterval){
+				ShootBullet();
+				shotDelay = 0;
+			}
+		}
+
 		if( m_Protectors == null || m_Protectors.Length < 1 )
 		{
 			return;
@@ -70,6 +102,38 @@ public class CharaController : MonoBehaviour
 			float z = m_ProtectorRadius * Mathf.Sin( angle );
 			m_Protectors[i].localPosition = new Vector3( x, 0, z );
 			m_Protectors[i].LookAt( transform );
+		}
+	}
+
+	private void ShootBullet(){
+		Debug.Log(string.Format("LV={0}", m_ShotLevel));
+		switch (m_ShotLevel){
+			case 1:
+				for(int i=0; i < m_MainShotPosition.Length; i++){
+					Instantiate(m_Bullet, m_MainShotPosition[i].position, m_MainShotPosition[i].rotation);
+				}
+				break;
+
+			case 2:
+				for(int i=0; i < m_MainShotPosition.Length; i++){
+					Instantiate(m_Bullet, m_MainShotPosition[i].position, m_MainShotPosition[i].rotation);
+				}
+				for(int i=0; i < m_SubShotPosition2.Length; i++){
+					Instantiate(m_Bullet, m_SubShotPosition2[i].position, m_SubShotPosition2[i].rotation);
+				}
+				break;
+
+			case 3:
+				for(int i=0; i < m_MainShotPosition.Length; i++){
+					Instantiate(m_Bullet, m_MainShotPosition[i].position, m_MainShotPosition[i].rotation);
+				}
+				for(int i=0; i < m_SubShotPosition2.Length; i++){
+					Instantiate(m_Bullet, m_SubShotPosition2[i].position, m_SubShotPosition2[i].rotation);
+				}
+				for(int i=0; i < m_SubShotPosition2.Length; i++){
+					Instantiate(m_Bullet, m_SubShotPosition3[i].position, m_SubShotPosition3[i].rotation);
+				}
+				break;
 		}
 	}
 }
