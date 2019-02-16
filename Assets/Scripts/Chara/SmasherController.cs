e1eefde2a774c9ed7b1b1095fdc56cd1a656bcb8
mstsh7 <hayakawagt400@gmail.com>
2019-02-16 22:21:45 +0900

    変更中 : SmasherController

--- Assets/Scripts/Chara/SmasherController.cs
+++ Assets/Scripts/Chara/SmasherController.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class SmasherController : PlayerController
 {
+    [SerializeField]
+    private Transform[] m_MainShotPosition;
+
+    [SerializeField]
+    private GameObject[] m_SubShotLv1;
+
+    private bool m_SubShotLv1CanShot;
+
+    [SerializeField]
+    private GameObject[] m_SubShotLv2;
+
+    private bool m_SubShotLv2CanShot;
+
+    [SerializeField, Range(1, 3)]
+    private int m_ShotLevel;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +32,12 @@ public class SmasherController : PlayerController
         
     }
 
-    public override void ShotBullet()
-    {
-        base.ShotBullet();
-        
+    public override void ShotBullet( int bulletIndex = 0)
+    { 
+        Bullet bullet = GetPoolBullet(0);
+        bullet.transform.position = transform.position;
+        bullet.transform.eulerAngles = Vector3.up * m_NowRad * Mathf.Rad2Deg;
+        bullet.transform.Translate(m_ShotPosOffset * Mathf.Cos(m_NowRad), 0, m_ShotPosOffset * Mathf.Sin(m_NowRad));
+        bullet.ShotBullet();
     }
 }
