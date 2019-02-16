88e207d2c725b782c15838d8f8f8615c1659eb7e
mstsh7 <hayakawagt400@gmail.com>
2019-02-16 20:49:04 +0900

    Update : Smasherの弾の撃ち方を変えた.

--- Assets/Scripts/Chara/CharaController.cs
+++ Assets/Scripts/Chara/CharaController.cs
@@ -36,10 +36,20 @@ public class CharaController : MonoBehaviour
 	private Transform[] m_MainShotPosition;
 
 	[SerializeField]
-	private Transform[] m_SubShotPosition2;
+	private GameObject[] m_SubShotLv2;
+
+	private bool m_SubShotLv2CanShot;
 
 	[SerializeField]
-	private Transform[] m_SubShotPosition3;
+	private GameObject[] m_SubShotLv3;
+
+	private bool m_SubShotLv3CanShot;
+
+	[SerializeField]
+	private float m_SubShotLv3Radius;
+
+	[SerializeField]
+	private float m_SubShotLv3RotateSpeed;
 
 	[SerializeField, Range(1,3)]
 	private int m_ShotLevel;
@@ -53,11 +63,32 @@ public class CharaController : MonoBehaviour
 	[SerializeField]
 	private float m_ProtectorSpeed;
 
-	private float m_Rad;
+	private float m_ProtectorRad;
+
+	private float m_SubShotLv3Rad;
 
 	private void Update()
 	{
 		shotDelay += Time.deltaTime;
+
+		if(m_ShotLevel >= 3){
+			m_SubShotLv3CanShot = true;
+			m_SubShotLv2CanShot = true;
+		}else if(m_ShotLevel >= 2){
+			m_SubShotLv3CanShot = false;
+			m_SubShotLv2CanShot = true;
+		}else{
+			m_SubShotLv3CanShot = false;
+			m_SubShotLv2CanShot = false;
+		}
+
+		for(int i=0; i< m_SubShotLv2.Length; i++){
+			m_SubShotLv2[i].SetActive(m_SubShotLv2CanShot);
+		}
+
+		for(int i=0; i< m_SubShotLv3.Length; i++){
+			m_SubShotLv3[i].SetActive(m_SubShotLv3CanShot);
+		}
 
 		if( Input.GetKey( m_FrontKey ) )
 		{
@@ -86,54 +117,53 @@ public class CharaController : MonoBehaviour
 			}
 		}
 
+		m_SubShotLv3Rad += m_SubShotLv3RotateSpeed * Time.deltaTime;
+		m_SubShotLv3Rad %= Mathf.PI * 2;
+		float unitAngle = Mathf.PI * 2 / m_SubShotLv3.Length;
+
+		if(m_SubShotLv3CanShot){
+			for(int i=0; i< m_SubShotLv3.Length; i++){
+				float angle = unitAngle * i + m_SubShotLv3Rad;
+				float x = m_SubShotLv3Radius * Mathf.Cos( angle );
+				float z = m_SubShotLv3Radius * Mathf.Sin( angle );
+				m_SubShotLv3[i].GetComponent<Transform>().localPosition = new Vector3( x, 0, z );
+				m_SubShotLv3[i].GetComponent<Transform>().LookAt( transform );
+			}
+		}
+
 		if( m_Protectors == null || m_Protectors.Length < 1 )
 		{
 			return;
 		}
 
-		m_Rad += m_ProtectorSpeed * Time.deltaTime;
-		m_Rad %= Mathf.PI * 2;
-		float unitAngle = Mathf.PI * 2 / m_Protectors.Length;
+		m_ProtectorRad += m_ProtectorSpeed * Time.deltaTime;
+		m_ProtectorRad %= Mathf.PI * 2;
+		unitAngle = Mathf.PI * 2 / m_Protectors.Length;
 
 		for( int i = 0; i < m_Protectors.Length; i++ )
 		{
-			float angle = unitAngle * i + m_Rad;
+			float angle = unitAngle * i + m_ProtectorRad;
 			float x = m_ProtectorRadius * Mathf.Cos( angle );
 			float z = m_ProtectorRadius * Mathf.Sin( angle );
 			m_Protectors[i].localPosition = new Vector3( x, 0, z );
 			m_Protectors[i].LookAt( transform );
-		}
+		}		
 	}
 
 	private void ShootBullet(){
-		Debug.Log(string.Format("LV={0}", m_ShotLevel));
-		switch (m_ShotLevel){
-			case 1:
-				for(int i=0; i < m_MainShotPosition.Length; i++){
+		for(int i=0; i < m_MainShotPosition.Length; i++){
 					Instantiate(m_Bullet, m_MainShotPosition[i].position, m_MainShotPosition[i].rotation);
 				}
-				break;
 
-			case 2:
-				for(int i=0; i < m_MainShotPosition.Length; i++){
-					Instantiate(m_Bullet, m_MainShotPosition[i].position, m_MainShotPosition[i].rotation);
+		if(m_SubShotLv2CanShot){
+			for(int i=0; i < m_SubShotLv2.Length; i++){
+					Instantiate(m_Bullet, m_SubShotLv2[i].GetComponent<Transform>().position, m_SubShotLv2[i].GetComponent<Transform>().rotation);
 				}
-				for(int i=0; i < m_SubShotPosition2.Length; i++){
-					Instantiate(m_Bullet, m_SubShotPosition2[i].position, m_SubShotPosition2[i].rotation);
-				}
-				break;
-
-			case 3:
-				for(int i=0; i < m_MainShotPosition.Length; i++){
-					Instantiate(m_Bullet, m_MainShotPosition[i].position, m_MainShotPosition[i].rotation);
-				}
-				for(int i=0; i < m_SubShotPosition2.Length; i++){
-					Instantiate(m_Bullet, m_SubShotPosition2[i].position, m_SubShotPosition2[i].rotation);
-				}
-				for(int i=0; i < m_SubShotPosition2.Length; i++){
-					Instantiate(m_Bullet, m_SubShotPosition3[i].position, m_SubShotPosition3[i].rotation);
-				}
-				break;
 		}
+		if(m_SubShotLv3CanShot){
+			for(int i=0; i < m_SubShotLv3.Length; i++){
+					Instantiate(m_Bullet, m_SubShotLv3[i].GetComponent<Transform>().position, m_SubShotLv3[i].GetComponent<Transform>().rotation);
+				}
+		}	
 	}
 }
