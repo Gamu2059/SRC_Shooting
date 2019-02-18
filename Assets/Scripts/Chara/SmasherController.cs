539a2f303a5e7670907b039262ee0b4b553b7441
mstsh7 <hayakawagt400@gmail.com>
2019-02-19 01:34:33 +0900

    Update : 自機たちの移動速度と弾のパラメータを変更

--- Assets/Scripts/Chara/SmasherController.cs
+++ Assets/Scripts/Chara/SmasherController.cs
@@ -49,7 +49,7 @@ public class SmasherController : PlayerController
              for (int i = 0; i < m_MainShotPosition.Length; i++)
             {
                 Bullet bullet = GetPoolBullet(0);   
-                bullet.ShotBullet(this, m_MainShotPosition[i].position, m_MainShotPosition[i].eulerAngles,  b.transform.localScale, bulletIndex, m_BulletParams[0], -1);
+                bullet.ShotBullet(this, m_MainShotPosition[i].position, m_MainShotPosition[i].eulerAngles,  b.transform.localScale, bulletIndex, m_BulletParams[0], 0);
             }
 
             if (m_SubShotLv1CanShot)
@@ -57,7 +57,7 @@ public class SmasherController : PlayerController
                 for (int i = 0; i < m_SubShotLv1.Length; i++)
                 {
                     Bullet bullet = GetPoolBullet(0);
-                    bullet.ShotBullet(this, m_SubShotLv1[i].transform.position, m_SubShotLv1[i].transform.eulerAngles, b.transform.localScale, bulletIndex, m_BulletParams[0], -1);
+                    bullet.ShotBullet(this, m_SubShotLv1[i].transform.position, m_SubShotLv1[i].transform.eulerAngles, b.transform.localScale, bulletIndex, m_BulletParams[0], 0);
                 }
             }
 
@@ -66,21 +66,12 @@ public class SmasherController : PlayerController
                 for (int i = 0; i < m_SubShotLv2.Length; i++)
                 {
                     Bullet bullet = GetPoolBullet(0);
-                    bullet.ShotBullet(this, m_SubShotLv2[i].transform.position, m_MainShotPosition[i].transform.eulerAngles, b.transform.localScale, bulletIndex, m_BulletParams[0], -1);
+                    bullet.ShotBullet(this, m_SubShotLv2[i].transform.position, m_MainShotPosition[i].transform.eulerAngles, b.transform.localScale, bulletIndex, m_BulletParams[0], 0);
 
                 }
             }
 
             shotDelay = 0;
-        }
-    }
-
-    private void Shoot(CharaControllerBase owner, Transform[] shooter, Bullet originBullet, int bulletIndex, BulletParam bulletParam, int orbitalIndex = -1)
-    {
-        for(int i=0; i < shooter.Length; i++)
-        {
-            Bullet bullet = GetPoolBullet(0);
-            bullet.ShotBullet(owner, shooter[i].position, shooter[i].eulerAngles, originBullet.transform.localScale, bulletIndex, bulletParam, orbitalIndex);
         }
     }
 
@@ -126,8 +117,8 @@ public class SmasherController : PlayerController
             for (int i = 0; i < m_SubShotLv2.Length; i++)
             {
                 float angle = unitAngle * i + m_SubShotLv2AngleDeg;
-                float x = m_SubShotLv2Radius * Mathf.Cos(-angle);
-                float z = m_SubShotLv2Radius * Mathf.Sin(-angle);
+                float x = m_SubShotLv2Radius * Mathf.Cos(angle * (i == 0 ? 1 : -1));
+                float z = m_SubShotLv2Radius * Mathf.Sin(angle * (i == 0 ? 1 : -1));
                 m_SubShotLv2[i].GetComponent<Transform>().localPosition = new Vector3(x, 0, z);
                 m_SubShotLv2[i].GetComponent<Transform>().LookAt(transform);
             }
