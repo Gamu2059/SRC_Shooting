283428d3d5a8c76066d059b086f23cebbc32ee6c
mstsh7 <hayakawagt400@gmail.com>
2019-02-28 03:22:18 +0900

    Add : いろいろ.

---
+++ Assets/Scripts/Chara/EchoController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EchoController : PlayerController
+{
+    [SerializeField, Range(0f, 1f)]
+    private float m_ShotInterval;
+
+    private float initialShotInterval;
+
+    [SerializeField]
+    private float m_ShotIntervalDecrease;
+
+    private float shotDelay;
+
+    [SerializeField]
+    private Transform[] m_MainShotPosition;
+
+    public void Awake()
+    {
+        initialShotInterval = m_ShotInterval;
+        OnAwake();
+    }
+
+    public override void OnAwake()
+    {
+        base.OnAwake();
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+        shotDelay += Time.deltaTime;
+    }
+
+    public override Bullet ShotBullet(int bulletIndex = 0, int bulletParamIndex = 0)
+    {
+        return base.ShotBullet(bulletIndex, bulletParamIndex);
+
+        /*if (shotDelay >= m_ShotInterval)
+        {
+            Bullet b = GetOriginalBullet(0);
+
+            for(int i=0; i< m_MainShotPosition.Length; i++)
+            {
+                Bullet bullet = GetPoolBullet(0);
+                bullet.ShotBullet(this, m_MainShotPosition[i].position, m_MainShotPosition[i].eulerAngles, b.transform.localScale, bulletIndex, m_BulletParams[0], 1);
+            }
+            shotDelay = 0;
+        }*/
+    }
+
+    public override void ShotBomb(int bombIndex = 0)
+    {
+        base.ShotBomb(bombIndex);
+    }
+
+    private void UpdateShotInterval(int level) {
+
+    }
+}
---
+++ Assets/Scripts/Chara/HackerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HackerController : PlayerController
+{
+    [SerializeField, Range(0f, 1f)]
+    private float m_ShotInterval;
+
+    private float shotDelay;
+
+    [SerializeField]
+    private Transform[] m_MainShotPosition;
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+        shotDelay += Time.deltaTime;
+    }
+
+    public override Bullet ShotBullet(int bulletIndex = 0, int bulletParamIndex = 0)
+    {
+
+        return base.ShotBullet(bulletIndex, bulletParamIndex);
+
+        /*
+        if (shotDelay >= m_ShotInterval && IsReadyShotBullet)
+        {
+            Bullet b = GetOriginalBullet(0);
+            for (int i = 0; i < m_MainShotPosition.Length; i++)
+            {
+                Bullet bullet = GetPoolBullet(0);
+                bullet.ShotBullet(this, m_MainShotPosition[i].position, m_MainShotPosition[i].eulerAngles, b.transform.localScale, bulletIndex, m_BulletParams[0], 2);
+            }
+            shotDelay = 0;
+            IsReadyShotBullet = false;
+        } 
+         */
+
+    }
+
+    public override void ShotBomb(int bombIndex = 0)
+    {
+        base.ShotBomb(bombIndex);
+    }
+}
