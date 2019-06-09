e8fd84f9304db1af0ded4a7983810b35bcd06724
mstsh7 <hayakawagt400@gmail.com>
2019-06-10 02:14:31 +0900

    E_DAMAGE_TYPE

---
+++ Assets/Scripts/Common/E_DAMAGE_TYPE.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum E_DAMAGE_TYPE
+{
+    // 一度のダメージ量が指定値となるタイプ
+    ONCE,
+
+    // 1秒間当たり続けた時の総ダメージ量が指定値となるタイプ
+    PER_SECOND,
+}
