9ea00d086be040ce3b3d06aa484b690312e65def
mstsh7 <hayakawagt400@gmail.com>
2019-09-30 03:17:52 +0900

    Add : EnemyGroupBehaviorParameter
    敵グループの生成パラメータとそれに付属するパラメータ

---
+++ Assets/Scripts/Param/Enemy/EnemyGenerationParameter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerationParameter : ScriptableObject
+{
+    [SerializeField]
+    private int m_HP;
+
+    // ドロップアイテム
+
+    // 撃破時イベント
+
+    [SerializeField]
+    private EnemyBehaviorParameter m_EnemyBehaviorParameter;
+}
