4cb2bd295ae36bcdc152d3624d1648f3df26142b
mstsh7 <hayakawagt400@gmail.com>
2019-09-30 03:17:52 +0900

    Add : EnemyGroupBehaviorParameter
    敵グループの生成パラメータとそれに付属するパラメータ

---
+++ Assets/Scripts/Param/Enemy/EnemyBehaviorParameter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviorParameter : ScriptableObject
+{
+    [SerializeField]
+    private Dictionary<string, GameObject> m_UseBulletPrefab;
+
+    [SerializeField]
+    private EnemyLookParameter m_EnemyLookParameter;
+}
