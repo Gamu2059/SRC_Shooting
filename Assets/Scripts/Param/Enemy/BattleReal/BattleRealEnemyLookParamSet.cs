72ca55de617d65ba536a93d521b0752c4fd70cbe
Gamu2059 <yamagami0711syo@gmail.com>
2019-10-08 19:42:53 +0900

    Update : 敵の生成パラメータを使いやすいように修正

---
+++ Assets/Scripts/Param/Enemy/BattleReal/BattleRealEnemyLookParamSet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// リアルモードの敵の見た目のパラメータのセット。
+/// </summary>
+[Serializable, CreateAssetMenu(menuName = "Param/BattleRealEnemy/EnemyLook", fileName = "param.battle_real_enemy_look")]
+public class BattleRealEnemyLookParamSet : ScriptableObject
+{
+    [SerializeField, Tooltip("敵のプールID これの名前が同じならば、プールから取得するモデルを再利用できます")]
+    private string m_EnemyId;
+    public string EnemyId => m_EnemyId;
+
+    [SerializeField, Tooltip("敵のプレハブ")]
+    private EnemyController m_EnemyPrefab;
+    public EnemyController EnemyPrefab => m_EnemyPrefab;
+}
