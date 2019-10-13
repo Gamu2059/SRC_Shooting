a36b39d0f4d027e54ab40818922e9d3bfde866e0
Gamu2059 <yamagami0711syo@gmail.com>
2019-10-14 01:15:04 +0900

    Update : 音声ファイルの差し替え

---
+++ Assets/Scripts/Adx2Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adx2Test : MonoBehaviour
+{
+    [SerializeField]
+    private CriWareInitializer m_Initializer;
+
+    [SerializeField]
+    private CriAtomSource criAtomSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Initializer.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
