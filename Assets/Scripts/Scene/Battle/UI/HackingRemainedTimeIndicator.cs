0d599bbcc6212641c14aa0dde9a3cb8f3639488d
mstsh7 <hayakawagt400@gmail.com>
2019-10-21 17:13:41 +0900

    Add : ハッキングモードのUI表示に必要なスクリプトの追加

---
+++ Assets/Scripts/Scene/Battle/UI/HackingRemainedTimeIndicator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+/// <summary>
+/// ハッキングモードの制限時間を表示するインジケーター
+/// </summary>
+public class HackingRemainedTimeIndicator : MonoBehaviour
+{
+    [SerializeField]
+    private Text m_OutText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_OutText.text = string.Format("{0}", 9999); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(BattleHackingManager.Instance == null){
+            return;
+        }   
+    }
+}
