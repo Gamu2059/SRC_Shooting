d1300e8b5559c14e5a63b69b8f72889a3909e45b
mstsh7 <hayakawagt400@gmail.com>
2019-11-09 23:19:15 +0900

    Change : InfC761BehaviorParamSetの内容の変更
    ・ボスの移動をインスペクタとスクリプトで指定するのに便利そうなのでBossMoveParamに待ち時間に関する変数を含めた
    ・移動のバリエーションのために複数のループ方法の選択肢を追加した
    ・移動用・弾発射用に使えるDurationを追加した

--- Assets/Scripts/Chara/BattleReal/Enemy/Base/BossMoveParam.cs
+++ Assets/Scripts/Chara/BattleReal/Enemy/Base/BossMoveParam.cs
@@ -13,4 +13,8 @@ public class BossMoveParam
     public Vector3 Destination;
 
     public E_COORD Coordinate;
+
+    public float NextMoveWaitTime;
+
+    public float MoveDuration;
 }
--- Assets/Scripts/Param/Enemy/BattleReal/InfC761/InfC761BehaviorParamSet.cs
+++ Assets/Scripts/Param/Enemy/BattleReal/InfC761/InfC761BehaviorParamSet.cs
@@ -9,13 +9,46 @@ using System;
 [Serializable, CreateAssetMenu(menuName = "Param/INF-C-761/PhaseN", fileName = "param.inf_c_761_phase_N.asset")]
 public class InfC761BehaviorParamSet : BattleRealBossBehaviorParamSet
 {
+    /// <summary>
+    /// ボスの移動についての設定
+    /// Destination[0],Destination[1]...Destination[N]
+    /// と移動した後どう動くか
+    /// </summary>
+    [Serializable]
+    public enum E_LOOP_SETTING{
+        /// <summary>
+        /// 一方通行
+        /// Destination[N]まで動いたらもう移動しない
+        /// </summary>
+        ONE_WAY,
+        /// <summary>
+        /// 往復
+        /// Destination[N],Destination[N-1]...Destination[0]と移動
+        /// </summary>
+        ROUND_TRIP,
+        /// <summary>
+        /// ループ
+        /// BasePosに戻りDestination[0]からの移動を再度繰り返す
+        /// </summary>
+        LOOP,
+    }
+
     [SerializeField]
     private Vector3 m_BasePos;
     public Vector3 BasePos => m_BasePos;
 
     [SerializeField]
+    private float m_StartDuration;
+    public float StartDuration => m_StartDuration;
+
+    [Header("Move Param")]
+    [SerializeField]
     private BossMoveParam[] m_MoveParams;
     public BossMoveParam[] MoveParams => m_MoveParams;
+
+    [SerializeField]
+    private E_LOOP_SETTING m_LoopSetting;
+    public E_LOOP_SETTING LoopSetting => m_LoopSetting;
 
     [SerializeField]
     private float m_Amplitude;
@@ -26,17 +59,9 @@ public class InfC761BehaviorParamSet : BattleRealBossBehaviorParamSet
     public AnimationCurve[] NormalizedRates => m_NormalizedRates;
 
     [SerializeField]
-    private float[] m_NextMoveWaitTimes;
-    public float[] NextMoveWaitTimes => m_NextMoveWaitTimes;
-
-    [SerializeField]
-    private float m_StartDuration;
-    public float StartDuration => m_StartDuration;
-
-    [SerializeField]
-    private float[] m_MoveDurations;
-    public float[] MoveDurations => m_MoveDurations;
-
+    private float[] m_GenericDurations;
+    public float[] GenericDurations => m_GenericDurations;
+    
     [Header("Shot Param")]
     [SerializeField]
     private float[] m_ShotCoolTimes;
