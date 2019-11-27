a00b798295764ac5b61c323f7d62cfcb9c64b74c
Gamu2059 <yamagami0711syo@gmail.com>
2019-11-27 12:00:05 +0900

    Update : ゲームオーバーアセットのインポート

---
+++ Assets/Scripts/CustomImageEffect.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CustomImageEffect : MonoBehaviour
+{
+    [SerializeField]
+    private Material m_Material;
+
+    [SerializeField]
+    private AnimationCurve m_ShifLevelCurve;
+
+    private bool m_IsPush = false;
+    private float m_Time = 0;
+
+    void Start()
+    {
+        m_Material.SetFloat("_ShiftLevel", 0);
+    }
+
+    void Update()
+    {
+
+        if (m_IsPush)
+        {
+            // 一定時間後にShiftLevelを上げる
+            var level = m_ShifLevelCurve.Evaluate(m_Time);
+
+            m_Time += Time.deltaTime;
+            // シェーダに数値を渡す
+            m_Material.SetFloat("_ShiftLevel", level);
+        }
+        else if (Input.anyKey)
+        {
+            m_IsPush = true;
+        }
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        // イメージエフェクトの適用
+        if (m_Material)
+            Graphics.Blit(source, destination, m_Material);
+    }
+}
---
+++ Assets/Scripts/TextTypingAnimator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTypingAnimator : MonoBehaviour {
+
+    [SerializeField]
+    private Text m_Text;
+
+    [SerializeField, Multiline]
+    private string m_TargetText;
+
+    [SerializeField]
+    private float m_TypingSpeed = 10;
+
+    [SerializeField]
+    private int m_ForwardIndexNum = 2;
+
+    private int m_CurrentIndex;
+    private float m_TimeCount;
+    private bool m_IsComplete;
+
+    private void Start () {
+        m_Text.text = "";
+        m_TimeCount = 0;
+        m_CurrentIndex = 0;
+        m_IsComplete = false;
+    }
+
+    private void Update () {
+
+        if (m_IsComplete)
+        {
+            return;
+        }
+
+        if (m_TimeCount >= m_TypingSpeed)
+        {
+            m_TimeCount -= m_TypingSpeed;
+            m_CurrentIndex += m_ForwardIndexNum;
+            if (m_CurrentIndex > m_TargetText.Length)
+            {
+                m_CurrentIndex = m_TargetText.Length;
+            }
+        }
+
+        m_TimeCount += Time.deltaTime;
+        m_Text.text = m_TargetText.Substring(0, m_CurrentIndex);
+        if (m_CurrentIndex >= m_TargetText.Length) {
+            m_IsComplete = true;
+        }
+    }
+}
