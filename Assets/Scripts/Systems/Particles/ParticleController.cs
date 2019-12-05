0a59cf2ee71157800ad4c6b3429f460ffa00d7f6
Gamu2059 <yamagami0711syo@gmail.com>
2019-12-06 08:11:12 +0900

    Update : 撃破エフェクトの追加

---
+++ Assets/Scripts/Systems/Particles/ParticleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : ControllableMonoBehavior
+{
+    [SerializeField]
+    private float m_Duration;
+
+    private float m_Time;
+
+    private void Start()
+    {
+        m_Time = 0;
+    }
+
+    private void Update()
+    {
+        m_Time += Time.deltaTime;
+        if (m_Time >= m_Duration)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
