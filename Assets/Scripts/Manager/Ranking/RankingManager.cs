0ff356afba93a1a23e34028d7114ed294db6b3be
mstsh7 <>
2020-02-03 01:45:00 +0900

    WIP

---
+++ Assets/Scripts/Manager/Ranking/RankingManager.cs
@@ -0,0 +1,95 @@
+﻿#pragma warning disable 0649
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RankingManager : ControllableMonoBehavior
+{
+    private enum E_RANKING_MENU_STATE
+    {
+        FORCUS_STORY,
+        SELECT_STORY,
+        FORCUS_CHAPTER,
+        SELECT_CHAPTER,
+        FORCUS_EXIT,
+        SELECT_EXIT,
+    }
+
+    [SerializeField]
+    private RankingUIManager m_UiManager;
+
+    [SerializeField]
+    private Transform m_Overall;
+
+    [SerializeField]
+    private PlaySoundParam m_CursorSe;
+
+    [SerializeField]
+    private PlaySoundParam m_OkSe;
+
+    [SerializeField]
+    private PlaySoundParam m_CancelSe;
+
+    [SerializeField]
+    private float m_WaitCursorTime;
+
+    private TitleInputManager InputManager;
+    private StateMachine<E_RANKING_MENU_STATE> m_StateMachine;
+
+    private bool m_EnableMenu;
+
+    #region Game Cycle
+
+    public override void OnInitialize()
+    {
+        base.OnInitialize();
+
+        m_StateMachine = new StateMachine<E_RANKING_MENU_STATE>();
+
+        m_StateMachine.AddState(new State<E_RANKING_MENU_STATE>(E_RANKING_MENU_STATE.FORCUS_STORY)
+        {
+            m_OnStart = StartOnFocusStory,
+            m_OnUpdate = UpdateOnFocusStory,
+            m_OnEnd = EndOnFocusStory,
+        });
+
+        m_StateMachine.AddState(new State<E_RANKING_MENU_STATE>(E_RANKING_MENU_STATE.FORCUS_CHAPTER)
+        {
+            m_OnStart = StartOnFocusChapter,
+            m_OnUpdate = UpdateOnFocusChapter,
+            m_OnEnd = EndOnFocusChapter,
+        });
+
+        m_StateMachine.AddState(new State<E_RANKING_MENU_STATE>(E_RANKING_MENU_STATE.FORCUS_EXIT)
+        {
+            m_OnStart = StartOnFocusEnd,
+            m_OnUpdate = UpdateOnFocusEnd,
+            m_OnEnd = EndOnFocusEnd,
+        });
+
+        m_StateMachine.AddState(new State<E_RANKING_MENU_STATE>(E_RANKING_MENU_STATE.SELECT_STORY) 
+        {
+            m_OnStart = StartOnSelectStory,
+            m_OnUpdate = UpdateOnSelectStory,
+            m_OnEnd = EndOnSelectStory,
+        });
+
+        m_StateMachine.AddState(new State<E_RANKING_MENU_STATE>(E_RANKING_MENU_STATE.SELECT_CHAPTER) 
+        { 
+            m_OnStart = StartOnSelectChapter,
+            m_OnUpdate = UpdateOnSelectChapter,
+            m_OnEnd = EndOnSelectChapter,
+        });
+
+        m_StateMachine.AddState(new State<E_RANKING_MENU_STATE>(E_RANKING_MENU_STATE.SELECT_EXIT) 
+        { 
+            m_OnStart = StartOnSelectExit,
+            m_OnUpdate = UpdateOnSelectExit,
+            m_OnEnd = EndOnSelectExit,
+        });
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Manager/Ranking/RankingUIManager.cs
@@ -0,0 +1,58 @@
+﻿#pragma warning disable 0649
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankingUIManager : ControllableMonoBehavior
+{
+    private const string MENU_ENABLE = "menu_enable";
+    private const string MENU_DISABLE = "menu_disable";
+    private const string MENU_ENABLE_FORCE = "menu_enable_force";
+    private const string MENU_DISABLE_FORCE = "menu_disable_force";
+
+    [SerializeField]
+    private Animator[] m_MenuAnimators;
+
+    private int m_EnableIdx;
+
+    public override void OnInitialize()
+    {
+        base.OnInitialize();
+        DisableAllMenuForce();
+        ForcusMenu(0, true);
+    }
+
+    public void DisableAllMenuForce()
+    {
+        foreach(var m in m_MenuAnimators)
+        {
+            m.Play(MENU_DISABLE_FORCE, 0);
+        }
+    }
+
+    private void EnableMenu(int idx)
+    {
+        var i = Mathf.Clamp(idx, 0, m_MenuAnimators.Length - 1);
+        var m = m_MenuAnimators[i];
+        m.Play(MENU_ENABLE_FORCE, 0);
+    }
+
+    private void DisableMenu(int idx)
+    {
+        var i = Mathf.Clamp(idx, 0, m_MenuAnimators.Length - 1);
+        var m = m_MenuAnimators[i];
+        m.Play(MENU_DISABLE, 0);
+    }
+
+    public void ForcusMenu(int idx, bool isForce = false)
+    {
+        if (m_EnableIdx != idx || isForce)
+        {
+            DisableMenu(m_EnableIdx);
+            EnableMenu(idx);
+            m_EnableIdx = idx;
+        }
+    }
+}
