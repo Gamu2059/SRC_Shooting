be27f5f988b3afd9b7cd88abaa2ea53e168de2f9
mstsh7 <>
2020-02-03 02:38:00 +0900

    Change : ランキングメニューを作成
    GameManagerにて最初に起動するシーンをRANKINGに変更

--- Assets/Scripts/Manager/Ranking/RankingManager.cs
+++ Assets/Scripts/Manager/Ranking/RankingManager.cs
@@ -21,9 +21,6 @@ public class RankingManager : ControllableMonoBehavior
     private RankingUIManager m_UiManager;
 
     [SerializeField]
-    private Transform m_Overall;
-
-    [SerializeField]
     private PlaySoundParam m_CursorSe;
 
     [SerializeField]
@@ -38,7 +35,7 @@ public class RankingManager : ControllableMonoBehavior
     private TitleInputManager InputManager;
     private StateMachine<E_RANKING_MENU_STATE> m_StateMachine;
 
-    private bool m_EnableMenu;
+    private bool m_EnableMove;
 
     #region Game Cycle
 
@@ -64,9 +61,9 @@ public class RankingManager : ControllableMonoBehavior
 
         m_StateMachine.AddState(new State<E_RANKING_MENU_STATE>(E_RANKING_MENU_STATE.FORCUS_EXIT)
         {
-            m_OnStart = StartOnFocusEnd,
-            m_OnUpdate = UpdateOnFocusEnd,
-            m_OnEnd = EndOnFocusEnd,
+            m_OnStart = StartOnFocusExit,
+            m_OnUpdate = UpdateOnFocusExit,
+            m_OnEnd = EndOnFocusExit,
         });
 
         m_StateMachine.AddState(new State<E_RANKING_MENU_STATE>(E_RANKING_MENU_STATE.SELECT_STORY) 
@@ -89,7 +86,232 @@ public class RankingManager : ControllableMonoBehavior
             m_OnUpdate = UpdateOnSelectExit,
             m_OnEnd = EndOnSelectExit,
         });
+
+        InputManager = new TitleInputManager();
+        InputManager.OnInitialize();
+        m_UiManager.OnInitialize();
+        m_StateMachine.Goto(E_RANKING_MENU_STATE.FORCUS_STORY);
+    }
+
+    public override void OnFinalize()
+    {
+        InputManager.RemoveInput();
+
+        m_UiManager.OnFinalize();
+        InputManager.OnFinalize();
+        m_StateMachine.OnFinalize();
+        base.OnFinalize();
+    }
+
+    public override void OnStart()
+    {
+        base.OnStart();
+        InputManager.OnStart();
+        InputManager.RegistInput();
+        m_EnableMove = true;
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+        InputManager.OnUpdate();
+        m_StateMachine?.OnUpdate();
     }
 
     #endregion
+
+    private void CheckForcusAction(Action onMoveUp, Action onMoveDown)
+    {
+        if (!m_EnableMove)
+        {
+            return;
+        }
+
+        var v = InputManager.MoveDir.y;
+        if (v > 0.8)
+        {
+            if (onMoveUp != null)
+            {
+                PlayCursor();
+                WaitCursor();
+                onMoveUp.Invoke();
+            }
+        }
+        else if (v < -0.8)
+        {
+            if (onMoveDown != null)
+            {
+                PlayCursor();
+                WaitCursor();
+                onMoveDown.Invoke();
+            }
+        }
+    }
+
+    private void WaitCursor()
+    {
+        m_EnableMove = false;
+        var t = Timer.CreateTimeoutTimer(E_TIMER_TYPE.UNSCALED_TIMER, m_WaitCursorTime, () => m_EnableMove = true);
+        TimerManager.Instance.RegistTimer(t);
+    }
+
+    private void CheckSelectAction(Action onSelect)
+    {
+        if (InputManager.Submit == E_INPUT_STATE.DOWN)
+        {
+            if (onSelect != null)
+            {
+                PlayOk();
+                onSelect.Invoke();
+            }
+        }
+    }
+
+    private void CheckCancelAction(Action onCancel)
+    {
+        if (InputManager.Cancel == E_INPUT_STATE.DOWN)
+        {
+            if (onCancel != null)
+            {
+                PlayCancel();
+                onCancel.Invoke();
+            }
+        }
+    }
+
+    private void PlayCursor()
+    {
+        AudioManager.Instance.Play(m_CursorSe);
+    }
+
+    private void PlayOk()
+    {
+        AudioManager.Instance.Play(m_OkSe);
+    }
+
+    private void PlayCancel()
+    {
+        AudioManager.Instance.Play(m_CancelSe);
+    }
+
+    #region Focus Story
+
+    private void StartOnFocusStory()
+    {
+        m_UiManager.ForcusMenu(0);
+    }
+
+    private void UpdateOnFocusStory()
+    {
+        CheckForcusAction(null, () => m_StateMachine.Goto(E_RANKING_MENU_STATE.FORCUS_CHAPTER));
+    }
+
+    private void EndOnFocusStory()
+    {
+
+    }
+
+    #endregion
+
+    #region Select Story
+
+    private void StartOnSelectStory()
+    {
+
+    }
+
+    private void UpdateOnSelectStory()
+    {
+
+    }
+
+    private void EndOnSelectStory()
+    {
+
+    }
+
+    #endregion
+
+    #region Focus Chapter
+
+    private void StartOnFocusChapter()
+    {
+        m_UiManager.ForcusMenu(1);
+    }
+
+    private void UpdateOnFocusChapter()
+    {
+        CheckForcusAction(
+            () => m_StateMachine.Goto(E_RANKING_MENU_STATE.FORCUS_STORY),
+            () => m_StateMachine.Goto(E_RANKING_MENU_STATE.FORCUS_EXIT));
+    }
+
+    private void EndOnFocusChapter()
+    {
+
+    }
+
+    #endregion
+
+    #region Select Chapter
+
+    private void StartOnSelectChapter()
+    {
+
+    }
+
+    private void UpdateOnSelectChapter()
+    {
+
+    }
+
+    private void EndOnSelectChapter()
+    {
+
+    }
+
+    #endregion
+
+    #region Focus Exit
+
+    private void StartOnFocusExit()
+    {
+        m_UiManager.ForcusMenu(2);
+    }
+
+    private void UpdateOnFocusExit()
+    {
+        CheckForcusAction(() => m_StateMachine.Goto(E_RANKING_MENU_STATE.FORCUS_CHAPTER), null);
+        CheckSelectAction(() => m_StateMachine.Goto(E_RANKING_MENU_STATE.SELECT_EXIT));
+    }
+
+    private void EndOnFocusExit()
+    {
+
+    }
+
+    #endregion
+
+    #region Select Exit
+
+    private void StartOnSelectExit()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_STANDALONE
+                Application.Quit();
+        #endif
+    }
+
+    private void UpdateOnSelectExit()
+    {
+
+    }
+
+    private void EndOnSelectExit()
+    {
+
+    }
+
+#endregion
 }
