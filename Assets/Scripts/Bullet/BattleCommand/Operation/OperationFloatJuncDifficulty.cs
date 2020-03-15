aacd8898ca1b3220e4518b020a63f7fbfb2336d4
Negoro1 <nkichiro25200302@gmail.com>
2020-03-16 00:32:23 +0900

    試しに1ボスの3回目の攻撃で、難易度によって弾幕のパラメータの一部を変えた。

--- Assets/Scripts/Bullet/BattleCommand/Operation/OperationFloatJuncDifficulty.cs
+++ Assets/Scripts/Bullet/BattleCommand/Operation/OperationFloatJuncDifficulty.cs
@@ -3,45 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 難易度によって条件分岐させ、int型の値を返す演算を表すクラス。
+/// 難易度によって条件分岐させ、float型の値を返す演算を表すクラス。
 /// </summary>
-[CreateAssetMenu(menuName = "Param/Danmaku/operation/int/junction/difficulty", fileName = "OperationIntJuncDifficulty", order = 0)]
+[CreateAssetMenu(menuName = "Param/Danmaku/operation/float/junction/difficulty", fileName = "OperationFloatJuncDifficulty", order = 0)]
 [System.Serializable]
-public class OperationIntJuncDifficulty : OperationIntBase
+public class OperationFloatJuncDifficulty : OperationFloatBase
 {
 
     /// <summary>
     /// 難易度がEasyだった場合の値
     /// </summary>
     [SerializeField]
-    private OperationIntBase m_Easy;
+    private OperationFloatBase m_Easy;
 
     /// <summary>
     /// 難易度がNormalだった場合の値
     /// </summary>
     [SerializeField]
-    private OperationIntBase m_Normal;
+    private OperationFloatBase m_Normal;
 
     /// <summary>
     /// 難易度がHardだった場合の値
     /// </summary>
     [SerializeField]
-    private OperationIntBase m_Hard;
+    private OperationFloatBase m_Hard;
 
     /// <summary>
     /// 難易度がHadesだった場合の値
     /// </summary>
     [SerializeField]
-    private OperationIntBase m_Hades;
+    private OperationFloatBase m_Hades;
 
 
     public override float GetResultFloat()
-    {
-        return GetResultInt();
-    }
-
-
-    public override int GetResultInt()
     {
         // 現在の難易度を取得する
         E_DIFFICULTY difficulty = DataManager.Instance.BattleData.Difficulty;
@@ -49,16 +43,16 @@ public class OperationIntJuncDifficulty : OperationIntBase
         switch (difficulty)
         {
             case E_DIFFICULTY.EASY:
-                return m_Easy.GetResultInt();
+                return m_Easy.GetResultFloat();
 
             case E_DIFFICULTY.NORMAL:
-                return m_Normal.GetResultInt();
+                return m_Normal.GetResultFloat();
 
             case E_DIFFICULTY.HARD:
-                return m_Hard.GetResultInt();
+                return m_Hard.GetResultFloat();
 
             case E_DIFFICULTY.HADES:
-                return m_Hades.GetResultInt();
+                return m_Hades.GetResultFloat();
 
             default:
                 return 0;
