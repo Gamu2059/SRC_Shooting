3d25d035d253256e58470df20a627a5178bcba6e
mstsh7 <>
2020-03-16 01:50:13 +0900

    Add : チャプターモードのメニュー表示と入力受付を実装

---
+++ Assets/Scripts/Scene/ChapterMenuScene.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChapterMenuScene : BaseScene
+{
+    public override void OnBeforeShow(Action onComplete)
+    {
+        OnInitializeManagers();
+        base.OnBeforeShow(onComplete);
+    }
+
+    public override void OnAfterHide(Action onComplete)
+    {
+        OnFinalizeManagers();
+        base.OnAfterHide(onComplete);
+    }
+}
