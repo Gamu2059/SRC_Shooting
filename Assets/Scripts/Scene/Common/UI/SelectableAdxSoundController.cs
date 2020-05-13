161b447320a28006c2416a6a0fab6153808e5270
Gamu2059 <yamagami0711syo@gmail.com>
2020-05-13 21:09:09 +0900

    Update : Play Storyボタンの挙動を実装

---
+++ Assets/Scripts/Scene/Common/UI/SelectableAdxSoundController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectableAdxSoundController : MonoBehaviour, ISelectHandler, ISubmitHandler
+{
+    [SerializeField]
+    private E_COMMON_SOUND m_SelectSound;
+
+    [SerializeField]
+    private E_COMMON_SOUND m_SubmitSound;
+
+    public void OnSelect(BaseEventData e)
+    {
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.Play(m_SelectSound);
+        }
+    }
+
+    public void OnSubmit(BaseEventData e)
+    {
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.Play(m_SubmitSound);
+        }
+    }
+}
---
+++ Assets/Scripts/Scene/Common/UI/SelectableSubmitToClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// SubmitアクションをClickアクションに変換する
+/// </summary>
+public class SelectableSubmitToClick : MonoBehaviour, ISubmitHandler
+{
+    public void OnSubmit(BaseEventData e)
+    {
+        ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
+    }
+}
