07eb580c9d65cf7139fd04a1aa84e9274fec87ce
mstsh7 <>
2020-06-11 16:02:04 +0900

    Add : stage6 Hades g001 g002 g003

--- Assets/Scripts/Chara/BattleReal/EnemyGroup/EnemyGenerator/AppearRandomPositionEnemyGenerator.cs
+++ Assets/Scripts/Chara/BattleReal/EnemyGroup/EnemyGenerator/AppearRandomPositionEnemyGenerator.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEditor;
+using Rewired;
+using System.Xml.Schema;
 
 namespace BattleReal.EnemyGenerator
 {
@@ -28,6 +30,21 @@ namespace BattleReal.EnemyGenerator
             /// </summary>
             SAME,
         }
+
+        /// <summary>
+        /// プレイヤーがビューポート中心から見てどの位置にいるか
+        /// </summary>
+        private enum E_PLAYERS_VIEWPORT_POSITION 
+        {
+            UPPER_LEFT,
+
+            UPPER_RIGHT,
+
+            LOWER_RIGHT,
+
+            LOWER_LEFT,
+        }
+
 
         #endregion
 
@@ -64,6 +81,12 @@ namespace BattleReal.EnemyGenerator
         #endregion
 
         #region Field
+
+        private bool m_IsCountOffset;
+        private float m_GenerateTimeCount;
+        private int m_GeneratedEnemyCount;
+        private List<BattleRealEnemyBase> m_GeneratedEnemies;
+
         #endregion
 
         #region Game Cycle
@@ -71,19 +94,201 @@ namespace BattleReal.EnemyGenerator
         protected override void OnStartGenerator()
         {
             base.OnStartGenerator();
+
+            m_IsCountOffset = true;
+            m_GenerateTimeCount = 0;
+            m_GeneratedEnemyCount = 0;
+            m_GeneratedEnemies = new List<BattleRealEnemyBase>();
         }
 
         protected override void OnLateUpdateGenerator()
         {
             base.OnLateUpdateGenerator();
+
+            if(GenerateNum < 1)
+            {
+                EnemyGroup.Destory();
+                return;
+            }
+
+            if(m_GeneratedEnemyCount >= GenerateNum && m_GeneratedEnemies.Count < 1)
+            {
+                EnemyGroup.Destory();
+                return;
+            }
+
+            m_GeneratedEnemies.RemoveAll(e => e.GetCycle() == E_POOLED_OBJECT_CYCLE.POOLED);
         }
 
         protected override void OnFixedUpdateGenerator()
         {
             base.OnFixedUpdateGenerator();
+
+            if(m_GeneratedEnemyCount >= GenerateNum)
+            {
+                return;
+            }
+
+            switch (GenerateTimingType) 
+            {
+                case E_GENERATE_TIMING_TYPE.INDIVIDUAL:
+                    OnUpdateIndivisualType();
+                    break;
+                case E_GENERATE_TIMING_TYPE.SAME:
+                    OnUpdateSameType();
+                    break;
+            }
+
         }
 
         #endregion
 
+        private void OnUpdateIndivisualType()
+        {
+            if (m_IsCountOffset)
+            {
+                if(m_GenerateTimeCount >= GenerateOffsetTime)
+                {
+                    m_IsCountOffset = false;
+                    m_GenerateTimeCount -= GenerateOffsetTime;
+                    m_GenerateTimeCount += GenerateInterval;
+                }
+                else
+                {
+                    m_GenerateTimeCount += Time.fixedDeltaTime;
+                }
+            }
+            else
+            {
+                if(m_GenerateTimeCount >= GenerateInterval)
+                {
+                    Generate();
+                    m_GeneratedEnemyCount--;
+                    m_GenerateTimeCount -= GenerateInterval;
+                }
+                m_GenerateTimeCount += Time.fixedDeltaTime;
+            }
+        }
+
+        private void OnUpdateSameType()
+        {
+            if (m_IsCountOffset)
+            {
+                if (m_GenerateTimeCount >= GenerateOffsetTime)
+                {
+                    m_IsCountOffset = false;
+                    for (var i = 0; i < GenerateNum; i++)
+                    {
+                        Generate();
+                        m_GeneratedEnemyCount++;
+                    }
+                }
+                else
+                {
+                    m_GenerateTimeCount += Time.fixedDeltaTime;
+                }
+            }
+        }
+
+        private void Generate()
+        {
+            var enemy = BattleRealEnemyManager.Instance.CreateEnemy(ParamSet);
+            
+            if(enemy == null)
+            {
+                return;
+            }
+
+            var enemyT = enemy.transform;
+            enemyT.SetParent(EnemyGroup.transform, false);
+
+            enemyT.localPosition = GetEnemyPosition();
+
+            m_GeneratedEnemies.Add(enemy);
+        }
+
+        private Vector3 GetEnemyPosition()
+        {
+            Vector2 min = BattleRealStageManager.Instance.MinLocalFieldPosition;
+            Vector2 max = BattleRealStageManager.Instance.MaxLocalFieldPosition;
+            Vector3 playerPos = BattleRealPlayerManager.Instance.Player.transform.position;
+
+            var res = GetPlayersViewPortPosition(playerPos, min, max);
+
+            if(res == E_PLAYERS_VIEWPORT_POSITION.UPPER_LEFT)
+            {
+                return new Vector3(UnityEngine.Random.Range(playerPos.x + m_OffsetRadius, max.x),
+                                   0,
+                                   UnityEngine.Random.Range(playerPos.z + m_OffsetRadius, min.y));
+            }
+            else if(res == E_PLAYERS_VIEWPORT_POSITION.UPPER_RIGHT)
+            {
+                return new Vector3(UnityEngine.Random.Range(playerPos.x + m_OffsetRadius, min.x),
+                                   0,
+                                   UnityEngine.Random.Range(playerPos.z + m_OffsetRadius, max.y));
+            }
+            else if(res == E_PLAYERS_VIEWPORT_POSITION.LOWER_RIGHT)
+            {
+                return new Vector3(UnityEngine.Random.Range(playerPos.x + m_OffsetRadius, max.x),
+                                   0,
+                                   UnityEngine.Random.Range(playerPos.z + m_OffsetRadius, min.y));
+            }
+            else
+            {
+                return new Vector3(UnityEngine.Random.Range(playerPos.x + m_OffsetRadius, min.x),
+                                   0,
+                                   UnityEngine.Random.Range(playerPos.z + m_OffsetRadius, max.y));
+            }
+        }
+
+        private E_PLAYERS_VIEWPORT_POSITION GetPlayersViewPortPosition(Vector3 playerPos3D, Vector2 min, Vector2 max)
+        {
+            var pos2D = new Vector2(playerPos3D.x, playerPos3D.z);
+            Vector2 center = (max - min) / 2.0f;
+
+            int h = CalcPositionRelationship(center, center + Vector2.up, pos2D);
+            int v = CalcPositionRelationship(Rotation(center, -90.0f * Mathf.Deg2Rad),
+                                             Rotation(center + Vector2.up, -90.0f * Mathf.Deg2Rad),
+                                             pos2D);
+
+            if (h == 1 && v == 1)
+            {
+                return E_PLAYERS_VIEWPORT_POSITION.UPPER_RIGHT;
+            }
+            else if (h == -1 && v == 1)
+            {
+                return E_PLAYERS_VIEWPORT_POSITION.UPPER_LEFT;
+            }
+            else if (h == -1 && v == -1)
+            {
+                return E_PLAYERS_VIEWPORT_POSITION.LOWER_LEFT;
+            }
+            else
+            {
+                return E_PLAYERS_VIEWPORT_POSITION.LOWER_RIGHT;
+            }
+        }
+
+        private Vector2 Rotation(Vector2 v, float rad)
+        {
+            return new Vector2(v.x * Mathf.Cos(rad) - v.y * Mathf.Sin(rad),
+                               v.x * Mathf.Sin(rad) + v.y * Mathf.Cos(rad));
+        }
+
+        private int CalcPositionRelationship(Vector2 a, Vector2 b, Vector2 p)
+        {
+            Vector2 v = p - a;
+            Vector2 w = b - a;
+            float res = w.x * v.y - w.y * v.x;
+
+            if (res >= 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
     }
 }
