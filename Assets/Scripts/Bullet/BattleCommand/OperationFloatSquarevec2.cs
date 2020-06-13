e96c0c16ea57afd3d13c547d8c8e613e07cc39a7
Negoro1 <nkichiro25200302@gmail.com>
2020-06-13 22:18:21 +0900

    最終面パートBのハッキング攻撃のノーマル難易度を新たに1パターン分作成した。

---
+++ Assets/Scripts/Bullet/BattleCommand/OperationFloatSquarevec2.cs
@@ -0,0 +1,28 @@
+﻿#pragma warning disable 0649
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Vector2値の絶対値を二乗したfloat値を返す演算を表すクラス。
+/// </summary>
+[CreateAssetMenu(menuName = "Param/Danmaku/operation/float/squarevec2", fileName = "OperationFloatSquarevec2", order = 0)]
+[System.Serializable]
+public class OperationFloatSquarevec2 : OperationFloatBase
+{
+
+    /// <summary>
+    /// 二乗する値
+    /// </summary>
+    [SerializeField]
+    private OperationVector2Base m_Value;
+
+
+    public override float GetResultFloat()
+    {
+        Vector2 value = m_Value.GetResultVector2();
+
+        return value.x * value.x + value.y * value.y;
+    }
+}
