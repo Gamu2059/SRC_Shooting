fc8adf54b364b6fe9ae3209cc72fe4c39958c603
Gamu2059 <yamagami0711syo@gmail.com>
2020-07-05 20:22:16 +0900

    Update : チャプターのランキングを再実装

---
+++ Assets/Scripts/Record/ChapterRankingElement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChapterRankingElement : MonoBehaviour
+{
+    [SerializeField]
+    private Text m_Rank;
+
+    [SerializeField]
+    private Text m_PlayerName;
+
+    [SerializeField]
+    private Text m_Score;
+
+    [SerializeField]
+    private Text m_Date;
+
+    public void SetRanking(int rank, PlayerRecord record)
+    {
+        if (record == null)
+        {
+            return;
+        }
+
+        m_Rank.text = rank.ToString();
+        m_PlayerName.text = record.PlayerName;
+        m_Score.text = record.FinalScoreToString();
+        m_Date.text = record.PlayedDate;
+    }
+}
